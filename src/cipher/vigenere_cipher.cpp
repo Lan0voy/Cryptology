2567863c93800e4aa3e7100888a8c9c645c9804b
Lan0voy <krakovskyi.vladyslav@gmail.com>
2026-07-29 03:39:51 +0300

    [Issue #15] finish encrypt-function for Vigenere Cipher

--- src/cipher/vigenere_cipher.cpp
+++ src/cipher/vigenere_cipher.cpp
@@ -2,28 +2,13 @@
 
 #include <string>
 #include <stdexcept>
+#include <cctype>
 
 #include <cryptolibrium/cipher/vigenere_cipher.hpp>
 
 namespace cryptolibrium {
 
-    cipher::VigenereCipher::VigenereCipher(const std::string &key) : key_(key) {
-        validateKey(key_);
-    }
-
-    
-    // Validating key for recomendations by Vigenere Cipher// vigenere_cipher.cpp
-
-#include <string>
-#include <stdexcept>
-
-#include <cryptolibrium/cipher/vigenere_cipher.hpp>
-
-namespace cryptolibrium {
-
-    cipher::VigenereCipher::VigenereCipher(const std::string &key) : key_(key) {
-        validateKey(key_);
-    }
+    cipher::VigenereCipher::VigenereCipher(const std::string &key) : key_(prepareKey(key)) { }
 
     
     // Validating key for recomendations by Vigenere Cipher
@@ -45,11 +30,13 @@ namespace cryptolibrium {
     
     // Normalize key value for Vigenere Cipher
      std::string cipher::VigenereCipher::prepareKey(const std::string& key) {
+        validateKey(key);
+
         std::string preparedKey;
         preparedKey.reserve(key.size());
 
         for (unsigned char c : key) {
-            preparedKey += static_cast<unsigned char>(std::toupper(c));
+            preparedKey += static_cast<char>(std::toupper(c));
         }
 
         return preparedKey;
@@ -58,21 +45,31 @@ namespace cryptolibrium {
     
     std::string cipher::VigenereCipher::encrypt(const std::string& plaintext) const {
         std::size_t plaintextLength = plaintext.length();
-        std::string keyStream;
-        keyStream.reserve(plaintextLength);
         std::size_t keyIndex = 0;
+        std::size_t engAlphabetSize = 26;
+
+        std::string encryptedText;
+
+        char engAlphabetBase = 'A';
+        char keyChar;
+        char plain;
 
         for(std::size_t i = 0; i < plaintextLength; ++i) {
             unsigned char c = plaintext[i];            
             const bool isLetter = (((c >= 'A') && (c <= 'Z')) || ((c >= 'a') && (c <= 'z')));
 
             if (isLetter) {
-                keyStream[i] = key_[keyIndex];
+                keyChar = key_[keyIndex];
                 keyIndex = (keyIndex + 1) % key_.length();
+
+                plain = std::toupper(c);
+
+                char encrypted = engAlphabetBase + (plain - engAlphabetBase + keyChar - engAlphabetBase) % engAlphabetSize;
             }
         }
 
-        std::string encryptedText;
+
+
 
         return encryptedText;
     }
@@ -81,59 +78,4 @@ namespace cryptolibrium {
     std::string cipher::VigenereCipher::decrypt(const std::string& ciphertext) const {
 
     }
-}
-    static void validateKey(const std::string& key) {
-        if (key.empty()) {
-            throw std::invalid_argument("Key cannot be empty");
-        }   
-
-        for (unsigned char c : key) {
-            const bool isUpperCase = (c >= 'A' && c <= 'Z');
-            const bool isLowerCase = (c >= 'a' && c <= 'z');
-
-            if (!isUpperCase && !isLowerCase) {
-                throw std::invalid_argument("Key must contain only English alphabet letters");
-            }
-        }
-    }
-
-    
-    // Normalize key value for Vigenere Cipher
-    static std::string prepareKey(const std::string& key) {
-        std::string preparedKey;
-        preparedKey.reserve(key.size());
-
-        for (unsigned char c : key) {
-            preparedKey += static_cast<unsigned char>(std::toupper(c));
-        }
-
-        return preparedKey;
-    }
-
-    
-    std::string encrypt(const std::string& plaintext) {
-        std::size_t plaintextLength = plaintext.length();
-        std::string keyStream;
-        keyStream.reserve(plaintextLength);
-        std::size_t keyIndex = 0;
-
-        for(std::size_t i = 0; i < plaintextLength; ++i) {
-            unsigned char c = plaintext[i];            
-            const bool isLetter = (((c >= 'A') && (c <= 'Z')) || ((c >= 'a') && (c <= 'z')));
-
-            if (isLetter) {
-                keyStream[i] = key_[keyIndex];
-                keyIndex = [keyIndex + 1] % key_.length();
-            }
-        }
-
-        std::string encryptedText;
-
-        return encryptedText;
-    }
-
-
-    std::string decrypt(const std::string& ciphertext) {
-
-    }
-}
+}    
